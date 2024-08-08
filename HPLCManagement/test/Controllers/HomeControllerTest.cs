a30624047ef0b8594bf7975767b31fe0794084fe
mahermere <92261210+mahermere@users.noreply.github.com>
2024-08-08 13:58:46 -0400

    Add files via upload

---
+++ HPLCManagement/test/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using WC.Services.Hplc;
+using WC.Services.Hplc.Controllers;
+
+namespace WC.Services.Hplc.Tests.Controllers
+{
+	[TestClass]
+	public class HomeControllerTest
+	{
+		[TestMethod]
+		public void Index()
+		{
+			// Arrange
+			HomeController controller = new HomeController();
+
+			// Act
+			ViewResult result = controller.Index() as ViewResult;
+
+			// Assert
+			Assert.IsNotNull(result);
+			Assert.AreEqual("Home Page", result.ViewBag.Title);
+		}
+	}
+}
