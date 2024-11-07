8a1b66136880623883eec8a519ec39408493db58
mahermere <92261210+mahermere@users.noreply.github.com>
2024-11-07 16:31:18 -0600

    Add files via upload

---
+++ WorkflowAndCollab.Services.Logging-main/src/WC.Services.Logging/Controllers/v1/LoggingController.cs
@@ -0,0 +1,98 @@
+﻿//  ------------------------------------------------------------------------------------------------
+//  <copyright>
+//    Copyright (c) CareSource, 2020-2022.  All rights reserved.
+// 
+//    WC.Services.Logging
+//    LoggingController.cs
+//  </copyright>
+//  ------------------------------------------------------------------------------------------------
+
+namespace WC.Services.Logging.Controllers.v1
+{
+	using System.Net;
+	using System.Text;
+	using Microsoft.AspNetCore.Mvc;
+	using WC.Services.Logging.Managers.v1;
+	using WC.Services.Logging.Models.v1;
+
+	[Route("api/v{version:apiVersion}/[controller]")]
+	[ApiVersion("1")]
+	[ApiController]
+	public class LogController : ControllerBase
+	{
+		private readonly ILoggingManager _manager;
+
+		public LogController(ILoggingManager manager)
+			=> _manager = manager;
+
+		[HttpPost]
+		public IActionResult Log(
+			[FromBody] Message message) =>
+			Log(
+				message,
+				message.Event.LogLevel);
+
+		private IActionResult Log(
+			Message message,
+			LogLevel logLevel)
+		{
+			string token = GetTokenFromHeader();
+			return CompleteResult(_manager.Log(message, logLevel, token));
+		}
+
+		[HttpPost("Trace")]
+		public IActionResult LogTrace([FromBody] Message message)
+			=> Log(message, LogLevel.Trace);
+
+		[HttpPost("Debug")]
+		public IActionResult LogDebug([FromBody] Message message)
+			=> Log(message, LogLevel.Debug);
+
+		[HttpPost("Info")]
+		public IActionResult LogInfo([FromBody] Message message)
+			=> Log(message, LogLevel.Information);
+
+		[HttpPost("Warning")]
+		public IActionResult LogWarning([FromBody] Message message)
+			=> Log(message, LogLevel.Warning);
+
+		[HttpPost("Error")]
+		public IActionResult LogError([FromBody] Message message)
+			=> Log(message, LogLevel.Error);
+
+		[HttpPost("Critical")]
+		public IActionResult LogCritical([FromBody] Message message)
+			=> Log(message, LogLevel.Critical);
+
+
+		private IActionResult CompleteResult(ILoggingResponse result) =>
+			result.Code switch
+			{
+				-1 => StatusCode(500 ,result),
+				0 => Ok(result),
+				4 => Unauthorized(result),
+				_ => BadRequest(result)
+			};
+
+		private string GetTokenFromHeader()
+		{
+			string? authHeader = Request.Headers["Authorization"];
+
+			if (authHeader != null)
+			{
+				string[] tokens = authHeader.Split(' ');
+				byte[] bytes = Convert.FromBase64String(tokens[1]);
+				string auth = Encoding.ASCII.GetString(bytes);
+
+				if (auth.StartsWith("splunk:", StringComparison.InvariantCultureIgnoreCase))
+				{
+					return auth.Split(':')[1];
+				}
+				
+				return auth;
+			}
+
+			return string.Empty;
+		}
+	}
+}
---
+++ WorkflowAndCollab.Services.Logging-main/src/WC.Services.Logging/Models/v1/Message.cs
@@ -0,0 +1,49 @@
+﻿//  ------------------------------------------------------------------------------------------------
+//  <copyright>
+//    Copyright (c) CareSource, 2020-2022.  All rights reserved.
+// 
+//    WC.Services.Logging
+//    Message.cs
+//  </copyright>
+//  ------------------------------------------------------------------------------------------------
+
+//#pragma warning disable CS8618
+
+namespace WC.Services.Logging.Models.v1
+{
+	using System.Text.Json.Serialization;
+	using Newtonsoft.Json;
+
+	public class Message
+	{
+		[JsonProperty("time")]
+		public long Time { get; set; } = DateTimeOffset.Now.ToUniversalTime().ToUnixTimeMilliseconds();
+
+		[JsonProperty("host")]
+		public string Host { get; set; } = null!;
+
+		[JsonProperty("source")]
+		public string Source { get; set; } = null!;
+
+		[JsonProperty("sourcetype")]
+		public string SourceType { get; set; } = null!;
+
+		[JsonProperty("event")]
+		public Event Event { get; set; } = null!;
+
+		[JsonProperty("fields")]
+		public IDictionary<string, string> Fields { get; set; } = new Dictionary<string, string>();
+	}
+
+	public class Event
+	{
+		/// <summary>Gets or sets the message.</summary>
+		/// <value>The message.</value>
+		[JsonProperty("message")]
+		public string Message { get; set; } = string.Empty;
+
+		/// <summary>Gets or sets the event log level</summary>
+		[JsonProperty("logLevel")]
+		public LogLevel LogLevel { get; set; } = LogLevel.Information;
+	}
+}
