2d20ffdef40f48a32efb92db9da41744e10f8323
mahermere <92261210+mahermere@users.noreply.github.com>
2024-11-07 16:29:30 -0600

    Delete WorkflowAndCollab.Services.Eligibility-main directory

--- WorkflowAndCollab.Services.Eligibility-main/src/WC.Services.Eligibility/Adapters/Interfaces/IMemberAdapter.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CareSource.WC.Services.Eligibility.Adapters.Interfaces
-{
-    public interface IMemberAdapter<TMemberModel>
-    {
-        TMemberModel GetMemberBySubscriberId(string subscriberId, string subscriberSuffix);
-    }
-}
